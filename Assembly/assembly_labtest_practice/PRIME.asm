4a5543f1101bbcd0ff9c7d6dac84ca9492b97904
Asif Bin Kabir <asifbinkabircse@gmail.com>
2022-05-23 11:17:53 +0600

    file added

--- Assembly/assembly_labtest_practice/PRIME.asm
+++ Assembly/assembly_labtest_practice/PRIME.asm
@@ -19,8 +19,43 @@ MAIN PROC
     MOV AH,01H
     INT 21H
     
-    MOV NUM,AL
+    CMP AL,'0'
+    JZ IS_NOT_PRIME
+    CMP AL,'1'
+    JZ IS_NOT_PRIME
+    
     SUB AL,'0'
+    MOV NUM,AL
+    
+    MOV CL,2
+    
+    ITERATE:
+        CMP CL,NUM
+        JGE IS_PRIME
+        MOV AH,00H
+        DIV CL
+        CMP AH,00H
+        JE IS_NOT_PRIME
+        MOV AL,NUM
+        INC CL
+        JMP ITERATE
+        
+    IS_PRIME:
+        LEA DX,PRIME
+        MOV AH,09H
+        INT 21H
+        JMP EXIT
+    IS_NOT_PRIME:
+        LEA DX,NOTPRIME
+        MOV AH,09H
+        INT 21H
+        JMP EXIT
+    EXIT:
+    MOV AH,4CH
+    INT 21H
+MAIN ENDP
+END MAIN
+    
     
     
     
